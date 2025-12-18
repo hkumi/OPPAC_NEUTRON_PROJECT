bcf28a9006a3f194c65f8fc51a1fd5458b6ceafe
hkumi <60217793+hkumi@users.noreply.github.com>
2025-12-18 14:24:52 +0100

    Add files via upload

--- include/SteppingAction.hh
+++ include/SteppingAction.hh
@@ -1,77 +1,42 @@
-//
-// ********************************************************************
-// * License and Disclaimer                                           *
-// *                                                                  *
-// * The  Geant4 software  is  copyright of the Copyright Holders  of *
-// * the Geant4 Collaboration.  It is provided  under  the terms  and *
-// * conditions of the Geant4 Software License,  included in the file *
-// * LICENSE and available at  http://cern.ch/geant4/license .  These *
-// * include a list of copyright holders.                             *
-// *                                                                  *
-// * Neither the authors of this software system, nor their employing *
-// * institutes,nor the agencies providing financial support for this *
-// * work  make  any representation or  warranty, express or implied, *
-// * regarding  this  software system or assume any liability for its *
-// * use.  Please see the license in the file  LICENSE  and URL above *
-// * for the full disclaimer and the limitation of liability.         *
-// *                                                                  *
-// * This  code  implementation is the result of  the  scientific and *
-// * technical work of the GEANT4 collaboration.                      *
-// * By using,  copying,  modifying or  distributing the software (or *
-// * any work based  on the software)  you  agree  to acknowledge its *
-// * use  in  resulting  scientific  publications,  and indicate your *
-// * acceptance of all terms of the Geant4 Software license.          *
-// ********************************************************************
-//
-//
-/// \file B4/B4a/include/SteppingAction.hh
-/// \brief Definition of the B4a::SteppingAction class
-
-#ifndef B4aSteppingAction_h
-#define B4aSteppingAction_h 1
+#ifndef STEPPINGACTION_HH
+#define STEPPINGACTION_HH
 
 #include "G4UserSteppingAction.hh"
-#include <vector>  
-#include <fstream>
 #include "globals.hh"
+#include <vector>
+
 namespace B4
 {
-  class DetectorConstruction;
-}
 
-namespace B4a
-{
-
+// Forward declarations within the same namespace
+class DetectorConstruction;
 class EventAction;
-
-/// Stepping action class.
-///
-/// In UserSteppingAction() there are collected the energy deposit and track
-/// lengths of charged particles in Absober and Gap layers and
-/// updated in EventAction.
 
 class SteppingAction : public G4UserSteppingAction
 {
 public:
-  SteppingAction(const B4::DetectorConstruction* detConstruction,
-                 EventAction* eventAction);
-  ~SteppingAction() override = default;
+    SteppingAction(const DetectorConstruction* detConstruction,
+                   EventAction* eventAction);
+    virtual ~SteppingAction();
 
-  void UserSteppingAction(const G4Step* step) override;
-  void SaveEventData(G4int eventID); 
+    virtual void UserSteppingAction(const G4Step* step);
+    
+    void FillReconstructedPosition(G4int eventID);
+    
+    // Arrays for storing positions
+    std::vector<G4double> fXBottom;
+    std::vector<G4double> fXTop;
+    std::vector<G4double> fYLeft;
+    std::vector<G4double> fYRight;
+
 private:
-  const B4::DetectorConstruction* fDetConstruction = nullptr;
-  EventAction* fEventAction = nullptr;
-  std::vector<double> fXBottom;
-  std::vector<double> fXTop;
-  std::vector<double> fYLeft;
-  std::vector<double> fYRight;
-  G4int fCurrentEventID;
-  
+    const DetectorConstruction* fDetConstruction;
+    EventAction* fEventAction;
+    
+    G4int fCurrentEventID;
+    
+    void SaveEventData(G4int eventID);
 };
 
 }
-
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
-
 #endif
